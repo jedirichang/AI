b6149af61cb63ffd0fb9401c36b868a203cca84c
Richang Sharma <jedirichang@gmail.com>
2026-09-06 18:35:18 +0000

    Day 2

---
+++ ai-engineering/src/ai_engineering/main.py
@@ -0,0 +1,7 @@
+from ai_engineering.core.config import settings
+def main() -> None:
+    print(f"AI Engineering project is running! : ({settings.app_name})")
+
+
+if __name__ == "__main__":
+    main()
